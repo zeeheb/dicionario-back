8628607010a822467f043ac1f3a9e6f403583f80
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-06-02 15:03:07 -0300

    get + param

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const db = require('mysql');
+const mysql = require('mysql');
 var config = require('./config');
 
 app.use(cors());
@@ -14,12 +14,41 @@ const db = mysql.createConnection({
     database: config.database
 })
 
-app.post('/palavra/cadastrar'), (req, res) => {
+app.post("/cadastrar", (req, res) => {
+    console.log(req.body);
     const palavra = req.body.palavra;
     const regiao = req.body.regiao;
 
-    db.query('INSERT INTO palavra ')
-}
+    db.query('INSERT INTO palavra (nome , regiao) VALUES (?, ?) ', [palavra, regiao], (err, result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.get("/palavras", (req, res) => {
+    db.query("SELECT * FROM palavra", (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.get("/palavra/:nome", (req, res) => {
+    const nome = req.params.nome;
+    db.query("SELECT * from palavra WHERE nome LIKE ? ", nome, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
 
 
 app.listen(3001, () => {
