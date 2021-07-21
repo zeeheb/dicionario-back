61bb80c44ab03ccb27af057ad9119a81ca575f31
José Eduardo Brandão <zefutbrandao@gmail.com>
2021-07-21 20:16:43 -0300

    consulta para painel adm

--- controllers/gridController.js
+++ controllers/gridController.js
@@ -11,7 +11,17 @@ const db = mysql.createConnection({
 })
 
 router.get("/palavras", (req, res) => {
-    db.query("SELECT * FROM palavra", (err, result) => {
+    db.query("select p.palavra, s.id_sinal, s.avaliacao, r.uf_regiao, c.id_config, pnt.nome_ponto, s.caminho " +
+        "from palavra p, sinal s, sinal_regiao sr, regiao r, config c, sinal_config sc, sinal_ponto sp, ponto pnt " +
+        "where s.id_palavra = p.id_palavra " +
+        "and sr.id_sinal = s.id_sinal " +
+        "and sr.id_regiao = r.id_regiao " +
+        "and sc.id_sinal = s.id_sinal " +
+        "and sc.id_config = c.id_config " +
+        "and sp.id_sinal = s.id_sinal " +
+        "and sp.id_ponto = pnt.id_ponto_artic " +
+        "and s.status = 0 " +
+        "order by id_sinal;", (err, result) => {
         if (err) {
             res.send(err);
         } else {
